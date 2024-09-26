0402305d9f19d95399098c4ef2114c226a891a36
Lumos2020 <lucas.i.gutierrez@gmail.com>
2024-09-26 14:20:50 -0300

    new vue portal

---
+++ kaizen2bi-app/src/components/charts/BarChart.vue
@@ -0,0 +1,47 @@
+<template>
+  <Bar
+    id="my-bar-chart"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+  import { Bar } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+  export default {
+    name: 'BarChart',
+    components: { Bar },
+    data() {
+      return {
+        chartData: {
+          labels: [ 'Ener', 'February', 'Marzo', 'Abril','Mayo','Junio','Julio','Agosto','Septiembre','Noviembre','Diciembre' ],
+          datasets: [
+                      {
+                        label: 'Facturacion',
+                        backgroundColor: '#f87979',
+                        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+                      },
+                      {
+                        label: 'Gastos',
+                        backgroundColor: '#337dff',
+                        data: [20, 10, 6, 19, 5, 20, 19, 40, 20, 10, 6, 5],
+                      },
+                  ],
+                  },
+        chartOptions: {
+            responsive: true,
+            scales: {
+              y:  {
+                max: 100,
+                min: 0
+              }
+            }  
+          }
+      }
+    }
+  }
+</script>
---
+++ kaizen2bi-app/src/components/charts/LineChart.vue
@@ -0,0 +1,47 @@
+<template>
+    <Line 
+    id="my-line-chart"
+    :data="chartData" 
+    :options="chartOptions" />
+</template>
+  
+<script>
+  
+    import { Chart as ChartJS,     CategoryScale,    LinearScale,    PointElement,    LineElement,    Title,    Tooltip,    Legend  } from 'chart.js'
+    import { Line } from 'vue-chartjs'
+  
+    ChartJS.register(    CategoryScale,    LinearScale,    PointElement,    LineElement,    Title,    Tooltip,    Legend  )
+  
+    export default {
+        name: 'LineChart',
+        components: { Line },
+        data() {
+        return {
+            chartData:{
+                labels: [ 'Ener', 'February', 'Marzo', 'Abril','Mayo','Junio','Julio','Agosto','Septiembre','Noviembre','Diciembre' ],
+          datasets: [
+                      {
+                        label: 'Facturacion',
+                        backgroundColor: '#f87979',
+                        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+                      },
+                      {
+                        label: 'Gastos',
+                        backgroundColor: '#337dff',
+                        data: [20, 10, 20, 19, 12, 45, 25, 79, 35, 25, 14, 7],
+                      },
+                  ],
+                  },
+                    chartOptions : {
+                        responsive: true,
+                        scales: {
+                            y:  {
+                                max: 100,
+                                min: 0
+                            }
+                }
+            }
+        }
+    }
+}
+</script>
